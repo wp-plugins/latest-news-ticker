62e55be1708bf8166563cf2ae45980a2d5b0d258
ShaneJones <ShaneJones@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-13 20:14:02 +0000

    git-svn-id: https://plugins.svn.wordpress.org/latest-news-ticker/tags/1@518681 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package Latest News Ticker
+ * @version 0.1
+ */
+/*
+Plugin Name: Latest News Ticker
+Plugin URI: http://www.latestnewsticker.com
+Description: Latest news ticker
+Author: Shane Jones
+Version: 0.1
+Author URI: http://profiles.wordpress.org/ShaneJones/
+*/
+
+include "functions.php";
+
+
+if (is_admin()){
+	
+    add_action('admin_menu' , 'sdj_lnt_admin_add_page');
+    add_action('admin_init' , 'sdj_lnt_admin_init');
+	
+} else {
+	add_action('init'       , 'sdj_lnt_scripts');
+    add_action('wp_footer'  , 'sdj_lnt_build_ticker');
+}
+
+register_activation_hook(__FILE__, 'sdj_lnt_defaults');
+
+?>
