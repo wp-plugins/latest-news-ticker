52ead46b4eaf54233ecd4eb58f51b804c8d55f54
ShaneJones <ShaneJones@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-21 22:45:51 +0000

    Updating to 1.33 - Minor bugfix for pushing everything to peadig.com
    git-svn-id: https://plugins.svn.wordpress.org/latest-news-ticker/tags/1.33@791534 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package Latest News Ticker
+ * @version 1.32
+ */
+/*
+Plugin Name: Latest News Ticker
+Plugin URI: http://www.latestnewsticker.com
+Description: With Latest News Ticker for WordPress you too can have a news ticker along the bottom of your site highlighting the latest posts on your site.
+Author: Shane Jones
+Version: 1.33
+Author URI: http://profiles.wordpress.org/ShaneJones/
+*/
+
+include "functions.php";
+
+
+if (is_admin()){
+	
+    add_action('admin_menu'  , 'sdj_lnt_admin_add_page');
+    add_action('admin_init'  , 'sdj_lnt_admin_init');
+	
+} else {
+	add_action('init'        , 'sdj_lnt_scripts');
+    add_action('wp_footer'   , 'sdj_lnt_build_ticker');
+}
+
+register_activation_hook(__FILE__, 'sdj_lnt_defaults');
+register_deactivation_hook(__FILE__, 'sdj_lnt_defaults_remove');
+
+?>
